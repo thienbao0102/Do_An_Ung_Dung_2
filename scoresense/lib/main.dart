2ef533cfabe51cfe81af5580138550a9452af297
Hoai-pc <machhoai1234@gmail.com>
2024-10-26 01:52:57 +0700

    Nãy quên sửa lại main

--- scoresense/lib/main.dart
+++ scoresense/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
-      home: const PersonalResultPage()
+      home: const HomePage()
     );
   }
 }
