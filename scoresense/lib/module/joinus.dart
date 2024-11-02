bb1108bdec5a3780ea7946c185b1e4ed3b725d1e
nganlinh2004 <nganlinhdanggiao@gmail.com>
2024-11-02 23:30:37 +0700

    respon

--- scoresense/lib/module/joinus.dart
+++ scoresense/lib/module/joinus.dart
@@ -5,80 +5,89 @@ class Joinus extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Center(
       child: Container(
-        width: MediaQuery.of(context).size.width,
+        width: screenWidth,
         color: const Color(0xFFCDF5FD),
         child: Center(
           child: Container(
-            padding: const EdgeInsets.only(top: 100, bottom: 100),
-            width: MediaQuery.of(context).size.width * 0.68,
+            padding: EdgeInsets.only(
+              top: screenWidth > 600 ? 100 : 50,
+              bottom: screenWidth > 600 ? 100 : 50,
+            ),
+            width: screenWidth > 600 ? screenWidth * 0.68 : screenWidth * 0.9,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                const Text(
+                Text(
                   'Newsletter',
                   style: TextStyle(
-                    fontSize: 14,
+                    fontSize: screenWidth > 600 ? 14 : 12,
                     fontWeight: FontWeight.bold,
-                    color: Color(0xFF0062FF),
+                    color: const Color(0xFF0062FF),
                     decoration: TextDecoration.none,
                   ),
                 ),
                 const SizedBox(height: 10.0),
-                const Text(
+                Text(
                   'JOIN US',
                   style: TextStyle(
-                    fontSize: 40,
+                    fontSize: screenWidth > 600 ? 40 : 24,
                     fontWeight: FontWeight.bold,
-                    color: Color(0xFF252B42),
+                    color: const Color(0xFF252B42),
                     decoration: TextDecoration.none,
                   ),
                 ),
                 const SizedBox(height: 10.0),
-                const Text(
+                Text(
                   'Problems trying to resolve the conflict between\nthe two major realms of Classical physics: Newtonian mechanics',
                   textAlign: TextAlign.center,
                   style: TextStyle(
-                    fontSize: 14,
-                    color: Color(0xFF737373),
+                    fontSize: screenWidth > 600 ? 14 : 12,
+                    color: const Color(0xFF737373),
                   ),
                 ),
-                const SizedBox(height: 60),
+                const SizedBox(height: 40),
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  padding: EdgeInsets.symmetric(horizontal: screenWidth > 600 ? 10.0 : 5.0),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Container(
-                        height: 50, // Đặt chiều cao cho khung "Your Email"
-                        width: 450, // Điều chỉnh chiều rộng ở đây
-                        child: const TextField(
-                          decoration: InputDecoration(
-                            hintText: 'Your Email',
-                            filled: true,
-                            fillColor: Colors.white,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(4),
-                                bottomLeft: Radius.circular(4),
-                              ),
-                              borderSide: BorderSide.none,
+                      // Flexible widget to allow the email input to take available space
+                      Flexible(
+                        child: Container(
+                          height: 50,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Your Email',
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(4),
+                                  bottomLeft: Radius.circular(4),
+                                ),
+                                borderSide: BorderSide.none,
+                                ),
                             ),
                           ),
                         ),
                       ),
-                      const SizedBox(width: 0.5), // Khoảng cách giữa hai khung
+                      const SizedBox(width: 0.5),
+                      // Fixed width for the button
                       Container(
-                        height: 48, // Đặt cùng chiều cao với khung "Your Email"
+                        height: 50,
+                        width: screenWidth > 600 ? 120 : 90, // Adjusted width for small screens
                         child: OutlinedButton(
                           onPressed: () {
                             // Implement "Subscribe" functionality here
                           },
                           style: OutlinedButton.styleFrom(
-                            padding: const EdgeInsets.symmetric(horizontal: 18), // Đặt padding cho nút
+                            padding: const EdgeInsets.symmetric(horizontal: 8),
                             side: const BorderSide(color: Color(0xFF0062FF)),
-                            backgroundColor: Color(0xFF0062FF),
+                            backgroundColor: const Color(0xFF0062FF),
                             shape: const RoundedRectangleBorder(
                               borderRadius: BorderRadius.only(
                                 topRight: Radius.circular(6),
--- scoresense/lib/module/review.dart
+++ scoresense/lib/module/review.dart
@@ -6,45 +6,53 @@ class Review extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Container(
-      width: MediaQuery.of(context).size.width,
+      width: screenWidth,
       color: Colors.white,
       child: Center(
         child: Container(
-          padding: const EdgeInsets.only(top: 100, bottom: 200),
-          width: MediaQuery.of(context).size.width * 0.68,
+          padding: EdgeInsets.only(
+            top: screenWidth > 600 ? 100 : 50,
+            bottom: screenWidth > 600 ? 200 : 100,
+          ),
+          width: screenWidth > 600 ? screenWidth * 0.68 : screenWidth * 0.9,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text(
+              Text(
                 'SCORESENSE',
                 style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xFF0062FF)),
+                  fontSize: screenWidth > 600 ? 14 : 12,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF0062FF),
+                ),
               ),
-              const Text(
+              Text(
                 'Each client is important',
                 style: TextStyle(
-                  fontSize: 40,
+                  fontSize: screenWidth > 600 ? 40 : 24,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              const Text(
-                'Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics ',
-                style: TextStyle(fontSize: 14, color: Color(0xFF737373)),
+              Text(
+                'Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics',
+                style: TextStyle(
+                  fontSize: screenWidth > 600 ? 14 : 12,
+                  color: Color(0xFF737373),
+                ),
               ),
-              const SizedBox(height: 80),
-              // Thay đổi Expanded thành Container với chiều cao cố định
-              Container(
-                height: 300, // Đặt chiều cao cụ thể cho Container
+              SizedBox(height: screenWidth > 600 ? 80 : 40),
+              SizedBox(
+                height: screenWidth > 600 ? 300 : 250,
                 child: ListView.builder(
                   scrollDirection: Axis.horizontal,
-                  itemCount: 3, // Số lượng đánh giá bạn muốn hiển thị
+                  itemCount: 3,
                   itemBuilder: (context, index) {
                     return Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 5),
-                      child: buildReviewCard(),
+                      child: buildReviewCard(screenWidth),
                     );
                   },
                 ),
@@ -56,19 +64,18 @@ class Review extends StatelessWidget {
     );
   }
 
-  Widget buildReviewCard() {
+  Widget buildReviewCard(double screenWidth) {
     return Container(
-      width: 300,
-      height: 280,
-      padding: const EdgeInsets.all(30),
+      width: screenWidth > 600 ? 300 : 200,
+      padding: const EdgeInsets.all(15),
       decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 251, 252, 252),
+        color: Color.fromARGB(255, 251, 252, 252),
         boxShadow: [
           BoxShadow(
             color: Colors.black.withOpacity(0.2),
             spreadRadius: 2,
             blurRadius: 8,
-            offset: const Offset(4, 6),
+            offset: Offset(4, 6),
           ),
         ],
       ),
@@ -82,7 +89,7 @@ class Review extends StatelessWidget {
             direction: Axis.horizontal,
             allowHalfRating: true,
             itemCount: 5,
-            itemSize: 20.0,
+            itemSize: screenWidth > 600 ? 20.0 : 15.0,
             itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
             itemBuilder: (context, _) => const Icon(
               Icons.star,
@@ -92,23 +99,23 @@ class Review extends StatelessWidget {
               print(rating);
             },
           ),
-          const SizedBox(height: 25),
-          const Text(
+          SizedBox(height: screenWidth > 600 ? 20 : 10),
+          Text(
             'Slate helps you see how many more days you need to work to reach your financial goal for the month and year.',
             textAlign: TextAlign.center,
             style: TextStyle(
-              fontSize: 16,
+              fontSize: screenWidth > 600 ? 16 : 12,
               fontWeight: FontWeight.normal,
               color: Color(0xFF737373),
             ),
           ),
-          const SizedBox(height: 30),
-          const Row(
+          SizedBox(height: screenWidth > 600 ? 20 : 10),
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
-                radius: 25,
-                backgroundImage: AssetImage('linh.jpg'), // Hình ảnh bạn muốn sử dụng
+                radius: screenWidth > 600 ? 25 : 20,
+                backgroundImage: AssetImage('linh.jpg'),
               ),
               SizedBox(width: 10),
               Column(
@@ -117,7 +124,7 @@ class Review extends StatelessWidget {
                   Text(
                     'Regina Miles',
                     style: TextStyle(
-                      fontSize: 14,
+                      fontSize: screenWidth > 600 ? 14 : 12,
                       fontWeight: FontWeight.bold,
                       color: Color(0xFF00A9FF),
                     ),
@@ -125,15 +132,15 @@ class Review extends StatelessWidget {
                   Text(
                     'Designer',
                     style: TextStyle(
-                      fontSize: 12,
+                      fontSize: screenWidth > 600 ? 12 : 10,
                       fontWeight: FontWeight.normal,
                       color: Color(0xFF252B42),
                     ),
                   ),
                 ],
-              )
+              ),
             ],
-          )
+          ),
         ],
       ),
     );
